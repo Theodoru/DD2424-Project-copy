1e57e0bd421aad81f98f72c3de7ee02aa330340c
Felix Gudéhn <felix.gudehn@gmail.com>
2021-05-14 14:54:04 +0200

    Attempt at food_wide_resnet.py

--- FoodH5Dataset.py
+++ FoodH5Dataset.py
@@ -7,13 +7,14 @@ from pathlib import Path
 
 class FoodH5Dataset(torch.utils.data.Dataset):
 
-    def __init__(self, root, download, url):
+    def __init__(self, root, download, url, transform=None):
         super(FoodH5Dataset, self).__init__()
 
         self.root = root
         self.url = url
         self.filename = ntpath.basename(url)
         self.filepath = f"{self.root}/{self.filename}"
+        self.transform = transform
 
         if Path(self.filepath).is_file():
             if download:
@@ -35,7 +36,14 @@ class FoodH5Dataset(torch.utils.data.Dataset):
         image = self.images[index, :, :, :]
         category_name = self.category_names[self.category[index]]
         category_idx = np.where(self.category_names == category_name)[0]
-        return (torch.from_numpy(image), torch.from_numpy(category_idx))
+
+        X = torch.from_numpy(image).long()
+        y = torch.from_numpy(category_idx).long()
+
+        if self.transform:
+            X = self.transform(X)
+
+        return (X, y)
     
     def __len__(self):
         return self.images.shape[0]
