2d7d6bae95bbf3b24b3081407f3a89ee832e3658
theodoru <theodoru@kth.se>
2021-05-17 16:05:25 +0200

    image folder loading done

--- loaddata.py
+++ loaddata.py
@@ -7,28 +7,23 @@ from torch.utils.data import DataLoader
 import numpy as np
 
 
-## initialization
 IMG_SHAPE = (32,32)
-BATCH_SIZE = 32
-SHUFFLE = True
-
 TEST_DIR = "../archive.nosync/images/test"
 TRAIN_DIR = "../archive.nosync/images/train"
 
-
-##Preprocessing from paper
+##Image preprocessing from CIFAR in paper
 def create_dataset(train):
     transform = T.Compose([
         T.Resize(IMG_SHAPE),
         T.ToTensor(),
-        T.Normalize(np.array([125.3, 123.0, 113.9]) / 255.0,
-                    np.array([63.0, 62.1, 66.7]) / 255.0),
+        T.Normalize(np.array([125.3, 123.0, 113.9]) / 255.0, ## Change normalization???
+                    np.array([63.0, 62.1, 66.7]) / 255.0), ## Change normalization???
     ])
     if train:
       transform = T.Compose([
-        T.Pad(4, padding_mode='reflect'),
+        T.Pad(4, padding_mode='reflect'), ##Change value to funtion of size
         T.RandomHorizontalFlip(),
-        T.RandomCrop(32),
+        T.RandomCrop(32), ## Change value to size???
         transform
         ])
 
@@ -42,5 +37,5 @@ def create_dataset(train):
 test_ds = create_dataset(False)
 train_ds = create_dataset(True)
 
-print("test images = ", len(test_ds))
-print("train images = ", len(train_ds))
+print("test images = ", test_ds.idx_to_class)
+#print("train images = ", train_ds)
