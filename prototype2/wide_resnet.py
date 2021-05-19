b5122f14ee002446cfdf5f3f4f480247a070d595
Mazen Mardini <mazen@mengate.se>
2021-05-19 18:05:17 +0200

    Update.

--- prototype2/wide_resnet.py
+++ prototype2/wide_resnet.py
@@ -2,9 +2,13 @@ import os
 
 import torch
 from torch import nn
+from torch import Tensor
+import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor, Compose, Resize, CenterCrop, Normalize
+from torchvision.models.resnet import BasicBlock, Bottleneck
+
 
 # Configuration
 BATCH_SIZE = 16
@@ -14,6 +18,45 @@ MODEL_ARCHITECTURE = 'wide_resnet50_2'
 
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+class Model(nn.Module):
+    def __init__(self,
+                 num_classes: int = 100):
+        super(Model, self).__init__()
+
+        self.inplanes = 32  # Why 32?
+
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+
+        # FIXME: Layers should be defined here:
+        # self.block1 = BasicBlock(self.inplanes, planes=32, stride=1)
+
+        self.bn = nn.BatchNorm2d(self.inplanes)
+
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+
+        self.fc = nn.Linear(32*32, num_classes)
+
+    def forward(self, x: Tensor) -> Tensor:
+        # x = self.conv1(x)  # What's the point of this first convolution?
+        #x = self.bn(x)  # Batch normalization
+        #x = self.relu(x)  # Why ReLu here?
+        #x = self.maxpool(x)  # Why maxpool here?
+
+        # x = self.block1(x)  # FIXME: Remove this and add layers.
+        #x = self.layer1(x)
+        #x = self.layer2(x)
+        #x = self.layer3(x)
+        #x = self.layer4(x)
+
+        #x = self.avgpool(x)  # Why avgpool here?
+        #x = torch.flatten(x, 1)  # Puts all features in x in a vector.
+        #x = self.fc(x)  # Linear layer to acquire class-probabilities.
+
+        return x
 
 
 def train(data_loader, model, loss_fn, optimizer):
@@ -88,7 +131,8 @@ def main():
     test_data_loader = DataLoader(test_data, batch_size=BATCH_SIZE)
 
     # Model
-    model = torch.hub.load('pytorch/vision:v0.9.0', MODEL_ARCHITECTURE, pretrained=False)
+    # model = torch.hub.load('pytorch/vision:v0.9.0', MODEL_ARCHITECTURE, pretrained=False)
+    model = Model(num_classes=len(training_data.classes))
     model.to(DEVICE)
     print(model)
 
